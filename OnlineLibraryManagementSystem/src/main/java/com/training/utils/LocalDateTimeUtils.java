b14cd92df6766542e6025df3c7bd76d85402bb0d
447383 <447383@PC333882.cts.com>
2016-09-15 17:19:22 +0530

    updates on util package

--- OnlineLibraryManagementSystem/src/main/java/com/training/utils/LocalDateTimeUtils.java
+++ OnlineLibraryManagementSystem/src/main/java/com/training/utils/LocalDateTimeUtils.java
@@ -1,14 +1,21 @@
 package com.training.utils;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
 
 /**
  * this class is responsible for LocaldateTime implementations
  * 
+ * it implements AttributeConverter to convert date formats from SQL to JAVA
+ * 
  * @author 447383
  *
  */
-public class LocalDateTimeUtils {
+
+@Converter(autoApply = true)
+public class LocalDateTimeUtils implements AttributeConverter<LocalDateTime, Timestamp> {
 	/**
 	 * this method is used to return system date and time
 	 * 
@@ -19,4 +26,23 @@ public class LocalDateTimeUtils {
 		return LocalDateTime.now();
 
 	}
+
+	/**
+	 * converts java LocalDateTime to Timestamp
+	 */
+	@Override
+	public Timestamp convertToDatabaseColumn(LocalDateTime attribute) {
+
+		return (attribute == null ? null : Timestamp.valueOf(attribute));
+	}
+
+	/**
+	 * converts sql Timestamp to java LocalDtaeTime
+	 */
+	@Override
+	public LocalDateTime convertToEntityAttribute(Timestamp dbData) {
+
+		return (dbData == null ? null : dbData.toLocalDateTime());
+	}
+
 }
