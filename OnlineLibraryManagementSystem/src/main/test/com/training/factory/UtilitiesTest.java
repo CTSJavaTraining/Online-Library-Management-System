2ccd88433dd38e91c70fe7187a02de0c4b8071b0
559207 <559207@PC334044.cts.com>
2016-09-27 19:13:34 +0530

    Added Junit test case for util class.

---
+++ OnlineLibraryManagementSystem/src/main/test/com/training/factory/UtilitiesTest.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.training.factory;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.training.utils.Utilities;
+
+/**
+ * @author 559207
+ *
+ */
+public class UtilitiesTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("Going to create object...");
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		assertEquals("US111112", Utilities.idGenerator("US", "US111111"));
+		assertEquals("MM00013", Utilities.idGenerator("MM", "MM00012"));
+		assertEquals("MM100013", Utilities.idGenerator("MM", "MM100012"));
+		assertEquals("L12345", Utilities.idGenerator("L", "LL12344"));
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test() {
+		assertEquals("MM1A00013", Utilities.idGenerator("MM", "MM1A0012"));
+	}
+
+}
